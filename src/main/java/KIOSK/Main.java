4137d170a84c2378fa5e29329e380ec72a83a96a
user <user@DESKTOP-BGIC6EH>
2019-12-17 19:49:51 +0900

    commit

---
+++ src/main/java/KIOSK/Main.java
@@ -0,0 +1,16 @@
+package KIOSK;
+
+import java.awt.Frame;
+
+public class Main {
+	
+	public static void main(String arg[]) {
+		
+		Frame f=new Frame();
+		f.setTitle("첫번째 프레임");
+		f.setBounds(150, 100, 300, 300);
+		f.setVisible(true);
+		
+	}
+
+}

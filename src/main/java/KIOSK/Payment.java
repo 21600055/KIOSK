af0aa30d3e5ab62f7d24b0c93529029b581d5f17
user <user@DESKTOP-BGIC6EH>
2020-01-19 18:17:09 +0900

    commit

--- src/main/java/KIOSK/Payment.java
+++ src/main/java/KIOSK/Payment.java
@@ -17,8 +17,9 @@ public class Payment extends JFrame{
 		
 		super("결제정보창");
 		paypanel();
-		//genderpanel();
+		genderpanel();
 		agepanel();
+		add(center,BorderLayout.CENTER);
 		setSize(1000,750);
 		setVisible(true);
 	}
@@ -37,24 +38,35 @@ public class Payment extends JFrame{
 		pay.add(Box.createVerticalStrut(30));
 		JPanel left=new JPanel(new BorderLayout());
 		left.setBorder(new TitledBorder(new EtchedBorder(),"결제수단"));
-		left.add(pay,BorderLayout.WEST);
-		add(left);
+		left.add(pay,BorderLayout.CENTER);
+		center.add(left);
 	}
 	public void genderpanel() {
 		
 		setLayout(new BorderLayout());
 		Box gender=Box.createVerticalBox();
 		gender.add(Box.createVerticalStrut(30));
-		gender.add(new JCheckBox("남"));
+		gender.add(new JCheckBox("남").setFont(new Font()));
 		gender.add(Box.createVerticalStrut(30));
 		gender.add(new JCheckBox("여"));
 		gender.add(Box.createVerticalStrut(30));
 		JPanel center=new JPanel(new BorderLayout());
 		center.setBorder(new TitledBorder(new EtchedBorder(),"성별"));
 		center.add(gender,BorderLayout.CENTER);
-		add(center);
+		this.center.add(center);
 	}
 	public void agepanel() {
 		
+		setLayout(new BorderLayout());
+		Box age=Box.createVerticalBox();
+		age.add(Box.createVerticalStrut(30));
+		age.add(new JCheckBox("어른"));
+		age.add(Box.createVerticalStrut(30));
+		age.add(new JCheckBox("학생"));
+		age.add(Box.createVerticalStrut(30));
+		JPanel right=new JPanel(new BorderLayout());
+		right.setBorder(new TitledBorder(new EtchedBorder(),"나이 및 명수"));
+		right.add(age,BorderLayout.CENTER);
+		center.add(right);
 	}
 }
